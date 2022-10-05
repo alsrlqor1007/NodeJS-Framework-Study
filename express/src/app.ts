923fcc550ae8a382da23e9bce784d1c0f9cb550f
mingi <skyup9393@gmail.com>
2022-10-05 17:04:10 +0900

    express + ts 개발 환경 셋업 & hello world!

--- express/src/app.ts
+++ express/src/app.ts
@@ -1,1 +1,14 @@
-console.log("hello world!!");
+import * as express from 'express';
+
+const app: express.Express = express(); // express 인스턴스
+// const app: express.Application = express();
+const port = 8000;
+
+// Router를 통해 요청을 받는다.
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.send('Hello World! - 클라이언트로 보내는 응답')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
