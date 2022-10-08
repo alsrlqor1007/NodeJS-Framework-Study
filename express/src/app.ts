14d906c34937f1cc97b19cf031a95c0f1693a67b
mingi <skyup9393@gmail.com>
2022-10-08 20:32:25 +0900

    express 싱글톤 패턴, 서비스 패턴

--- express/src/app.ts
+++ express/src/app.ts
@@ -2,18 +2,54 @@ import * as express from 'express';
 import { Cat } from './cats/cats.model';
 import catsRouter from './cats/cats.route';
 
+// 싱글톤 패턴 적용
+class Server {
+    public app: express.Application;
+
+    constructor() {
+        const app: express.Application = express();
+        this.app = app;
+    }
+
+    private setRoute() {
+        this.app.use(catsRouter);
+    }
+
+    private setMiddleware() {
+        this.app.use((req, res, next) => {
+            // middleware를 거쳐서 라우터로 보낸다.
+            // 순서나 위치에 따라 미들웨어의 적용 범위가 다르다.
+            console.log(req.rawHeaders[1]); // 어디서 요청보냈는지 확인
+            console.log('this is logging middleware');
+            // next: 다음 라우터로 이동하는 함수
+            next();
+        })
+
+        // json middleware: express에서 json을 읽을 수 있도록 하는 미들웨어 추가
+        this.app.use(express.json());
+
+        this.setRoute();
+
+        // 존재하지 않는 라우터로 요청을 받았을 경우를 처리하는 미들웨어
+        // 404 middleware
+        this.app.use((req, res, next) => {
+            console.log('this is error middleware');
+            res.send({ error: '404 not found error' });
+        })
+    }
+
+    public listen() {
+        this.setMiddleware();
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`)
+        })
+    };
+}
+
 const app: express.Express = express(); // express 인스턴스
 // const app: express.Application = express();
 const port = 8000;
 
-app.use((req, res, next) => {
-    // middleware를 거쳐서 라우터로 보낸다.
-    // 순서나 위치에 따라 미들웨어의 적용 범위가 다르다.
-    console.log(req.rawHeaders[1]); // 어디서 요청보냈는지 확인
-    console.log('this is logging middleware');
-    // next: 다음 라우터로 이동하는 함수
-    next();
-})
 
 // 특정 라우터에만 미들웨어를 적용하는 또 다른 방법
 // logging middleware
@@ -22,13 +58,6 @@ app.get('/cats/som', (req, res, next) => {
     console.log('this is som middleware');
     next();
 })
-
-// json middleware: express에서 json을 읽을 수 있도록 하는 미들웨어 추가
-app.use(express.json());
-
-app.use(catsRouter);
-
-
 
 // Router를 통해 요청을 받는다.
 app.get('/', (req: express.Request, res: express.Response) => {
@@ -43,15 +72,9 @@ app.get('/cats/som', (req, res) => {
     res.send({ som: Cat[1] });
 })
 
+function init() {
+    const server = new Server();
+    server.listen();
+}
 
-
-// 존재하지 않는 라우터로 요청을 받았을 경우를 처리하는 미들웨어
-// 404 middleware
-app.use((req, res, next) => {
-    console.log('this is error middleware');
-    res.send({ error: '404 not found error' });
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+init();
--- express/src/cats/cats.route.ts
+++ express/src/cats/cats.route.ts
@@ -1,142 +1,25 @@
-import { Cat } from "./cats.model";
 import { Router } from 'express';
+import {createCat, deleteCat, readAllCat, readCat, updateCat, updatePartialCat} from "./cats.service";
 
 const router = Router();
 
 // READ 고양이 전체 데이터 모두 조회
-router.get('/cats', (req, res) => {
-    try {
-        const cats = Cat;
-        // throw new Error('db connect error');
-        res.status(200).send({
-            success: true,
-            data: {
-                cats,
-            }
-        });
-    } catch (error) {
-        res.status(400).send({
-            success: false,
-            error: error.message,
-        })
-    }
-})
+router.get('/cats', readAllCat);
 
 // READ 특정 고양이 데이터 조회
 // 동적 라우팅
-router.get('/cats/:id', (req, res) => {
-    try {
-        const params = req.params;
-        const cats = Cat.find((cat) => {
-            return cat.id === params.id;
-        });
-        // throw new Error('db connect error');
-        res.status(200).send({
-            success: true,
-            data: {
-                cats,
-            }
-        });
-    } catch (error) {
-        res.status(400).send({
-            success: false,
-            error: error.message,
-        })
-    }
-})
+router.get('/cats/:id', readCat);
 
 // CREATE 새로운 고양이 추가
-router.post('/cats', (req, res) => {
-    try {
-        const data = req.body;
-        Cat.push(data);
-
-        res.status(200).send({
-            success: true,
-            data: { data },
-        });
-    } catch (error) {
-        res.status(400).send({
-            success: false,
-            error: error.message,
-        });
-    }
-})
+router.post('/cats', createCat);
 
 // UPDATE 고양이 데이터 업데이트 -> PUT
-router.put('/cats/:id', (req, res) => {
-    try {
-        const params = req.params;
-        const body = req.body;
-
-        let result;
-        Cat.forEach((cat) => {
-            if (cat.id === params.id) {
-                cat = body;
-                result = cat;
-            }
-        })
-
-        res.status(200).send({
-            success: true,
-            data: {
-                result,
-            }
-        });
-    } catch (error) {
-        res.status(400).send({
-            success: false,
-            error: error.message,
-        })
-    }
-})
+router.put('/cats/:id', updateCat);
 
 // UPDATE 고양이 데이터 부분적으로 업데이트 -> PATCH
-router.patch('/cats/:id', (req, res) => {
-    try {
-        const params = req.params;
-        const body = req.body;
-
-        let result;
-        Cat.forEach((cat) => {
-            if (cat.id === params.id) {
-                cat = { ...cat, ...body }; // 구조 분해 할당
-                result = cat;
-            }
-        })
-
-        res.status(200).send({
-            success: true,
-            data: {
-                result,
-            }
-        });
-    } catch (error) {
-        res.status(400).send({
-            success: false,
-            error: error.message,
-        })
-    }
-})
+router.patch('/cats/:id', updatePartialCat);
 
 // DELETE 고양이 데이터 삭제 -> DELETE
-router.delete('/cats/:id', (req, res) => {
-    try {
-        const params = req.params;
-        const newCat = Cat.filter((cat) => cat.id !== params.id);
-
-        res.status(200).send({
-            success: true,
-            data: {
-                newCat,
-            }
-        });
-    } catch (error) {
-        res.status(400).send({
-            success: false,
-            error: error.message,
-        })
-    }
-})
+router.delete('/cats/:id', deleteCat);
 
 export default router;
