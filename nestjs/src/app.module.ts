ac61a7e77ce4fafbd48ae611c094683cccd947ba
mingi <skyup9393@gmail.com>
2022-10-12 23:56:56 +0900

    [보충] 캡슐화 추가 설명

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -16,5 +16,11 @@ import { UsersModule } from './users/users.module';
 
 - imports의 모듈에서 export를 한 것들을 app.module에서 사용할 수 있다.
 - 캡슐화: 모듈은 기본적으로 provider를 캡슐화한다. exports 하지 않은 provider는 사용할 수 없다.
+
+- providers에 DI 주입을 위해 모든 Service와 Repository들을 하나씩 넣어주는 것에는 단점이 있다.
+- 모듈들이 모두 분리가 되어 있는데 이를 무시하고 각각 직접 provider에 주입한다면 단일책임원칙에 위배된다고 할 수 있다.
+- 예를 들어 CatsService를 별도로 만들었는데, 은닉화가 되어 있어서 다른 모듈에서 접근을 못한다.
+- 접근이 가능하도록 exports 처리해주면 사용하고자 하는 타 모듈에서 providers에 추가해줄 필요 없이 exports 된 것을 사용할 수 있다.
+- providers에는 해당 모듈에서 만든 service나 gateway 등을 넣는 것이 좋고, 다른 모듈에서 만든 provider는 exports를 통해 public으로 만들어 사용한다.
 */
 export class AppModule {}
--- nestjs/src/cats/cats.module.ts
+++ nestjs/src/cats/cats.module.ts
@@ -7,5 +7,6 @@ import { CatsService } from './cats.service';
   providers: [CatsService],
   exports: [CatsService],
   // 캡슐화: 모듈은 기본적으로 provider를 캡슐화한다. exports 하지 않은 provider는 사용할 수 없다.
+  // provider는 원래 캡술화가 되어 있기 때문에 기본적으로는 다른 모듈에서 원래 사용할 수 없다.
 })
 export class CatsModule {}
