5d5433fab42a74aee6d69b4a3abf82bdb0aa759f
mingi <skyup9393@gmail.com>
2022-10-14 17:15:14 +0900

    Interceptors & AOP 패턴

--- nestjs/src/common/interceptors/success.interceptor.ts
+++ nestjs/src/common/interceptors/success.interceptor.ts
@@ -7,6 +7,12 @@ import {
 import { Observable } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
 
+/*
+Interceptor란, AOP(관점 지향 프로그래밍)에서 영향, DI 가능
+관점 지향 프로그래밍이란, 모듈성을 증가시키는 것이 목표인 프로그래밍 패러다임
+각각의 핵심 기능을 횡단하면서 재사용이 가능한 기능들을 관점 지향으로 하나로 묶는다.
+*/
+
 @Injectable()
 export class SuccessInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
