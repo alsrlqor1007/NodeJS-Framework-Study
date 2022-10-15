5ccfdafe190d49a4de210d249105e5218495cf4b
mingi <skyup9393@gmail.com>
2022-10-15 22:00:44 +0900

    DB 스키마, Controller 설계 & validation

---
+++ project_a/src/cats/cats.schema.ts
@@ -0,0 +1,35 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, SchemaOptions } from 'mongoose';
+import {IsEmail, IsNotEmpty, IsString} from "class-validator";
+
+const options: SchemaOptions = {
+  timestamps: true,
+};
+
+@Schema(options) // 스키마 데코레이터로 생성
+export class Cat extends Document {
+  // mongoose의 Document를 확장해서 Cat 생성
+  @Prop({
+    required: true,
+    unique: true,
+  })
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @Prop({ required: true })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @Prop({ required: true })
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @Prop()
+  @IsString()
+  imgUrl: string;
+}
+
+export const CatSchema = SchemaFactory.createForClass(Cat); // 정의한 Cat 클래스를 스키마로
