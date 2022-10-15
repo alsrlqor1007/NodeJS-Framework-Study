6fd0b19487f573ec05cf800b99da5894fd612784
mingi <skyup9393@gmail.com>
2022-10-15 22:08:03 +0900

    DB 스키마, Controller 설계 & validation

---
+++ project_a/src/cats/cats.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Controller,
+  Get,
+  Post,
+  UseFilters,
+  UseInterceptors,
+} from '@nestjs/common';
+import { CatsService } from './cats.service';
+import { HttpExceptionFilter } from '../common/exceptions/http-exception.filter';
+import { SuccessInterceptor } from '../common/interceptors/success.interceptor';
+
+@Controller('cats')
+@UseInterceptors(SuccessInterceptor)
+@UseFilters(HttpExceptionFilter)
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Get()
+  getCurrentCat() {
+    return 'current cat';
+  }
+
+  @Post()
+  async signUp() {
+    return 'signup';
+  }
+
+  @Post('login')
+  logIn() {
+    return 'login';
+  }
+
+  @Post('logout')
+  logOut() {
+    return 'logout';
+  }
+
+  @Post('upload/cats')
+  uploadCatImg() {
+    return 'uploadImg';
+  }
+}
