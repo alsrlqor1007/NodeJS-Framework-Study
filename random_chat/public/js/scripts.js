1d5dca14f5223ad8f76e42cb3770d806e73fc1bb
alsrlqor <skyup9393@gmail.com>
2022-10-22 23:21:00 +0900

    유니캐스팅 (Unicasting) : emit & on

--- random_chat/public/js/scripts.js
+++ random_chat/public/js/scripts.js
@@ -9,6 +9,17 @@ const formElement = getElementById('chat_form');
 
 function helloUser() {
   const username = prompt('What is your name?');
+
+  // socket으로 username을 보낸다. new_user라는 이벤트를 등록한 것.
+  socket.emit('new_user', username, (data) => {
+    console.log(data);
+  });
+  console.log(username);
+
+  // .on으로 데이터를 받고, 받으면 콟백함수 실행
+  socket.on('hello_user', (data) => {
+    console.log(data);
+  });
 }
 
 function init() {
--- random_chat/src/chats/chats.gateway.ts
+++ random_chat/src/chats/chats.gateway.ts
@@ -1,9 +1,22 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway()
 export class ChatsGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
+  @SubscribeMessage('new_user') // 서버에 socket이 들어오게 되면 new_user라는 이벤트를 찾는다.
+  handleNewUser(
+    @MessageBody() username: string, // 어떤 데이터를 받을건지
+    @ConnectedSocket() socket: Socket, // 소켓을 받음. emit과 on 사용
+  ): string {
+    console.log(username);
+    console.log(socket.id); // socket id. 웹 새로고침을 하면 socket은 끊기고 새로운 id를 부여받는다. id를 통해 접속한 클라이언트를 식별한다.
+
+    socket.emit('hello_user', 'hello ' + username);
     return 'Hello world!';
   }
 }
