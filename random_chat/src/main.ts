dcf3cafd848688ee0df23a38ee70b6727de1e6a3
alsrlqor <skyup9393@gmail.com>
2022-10-22 11:49:24 +0900

    HTTP vs Socket

--- random_chat/src/main.ts
+++ random_chat/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'public')); // js, css 파일 등 serving 가능하도록
-  app.setBaseViewsDir(join(__dirname, '..', 'views')); // 사용할 템플릿 엔진 뷰 위치. 템플릿 엔진이랑 HTML을 렌더링 할 수 있는 엔진
+  app.setBaseViewsDir(join(__dirname, '..', 'views')); // 사용할 템플릿 엔진 뷰 위치. 템플릿 엔진이란 HTML을 렌더링 할 수 있는 엔진
   app.setViewEngine('hbs'); // 사용하는 뷰 엔진
 
   await app.listen(3000);
